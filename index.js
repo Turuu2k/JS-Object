6f67f9e112e917fbc56c8317808eb3fe0d23b481
Indra <115783796+IngaIndra@users.noreply.github.com>
2024-07-26 16:57:17 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,38 @@
+// 1. Нохой нэртэй хоосон объект үүсгэ
+// Нохойн объектыг консол дээр хэвлэ
+// Нохойны объектын үүлдэр, нэр, өнгө, нас, хайр татсан шинж чанарыг нэмнэ үү. 
+// Нохойн объектоос үүлдэр, нэр, хөл, өнгө, нас, хайр татсан шинж чанарын value - г аваарай
+
+// 2. Машин нэртэй объект  
+// let car = {
+//   make: "Toyota",
+//   model: "Corolla",
+//   year: 2019,
+//   color: "Red"
+// };
+// 2.1 "color" property-г солих
+// 2.2 "owner" нэртэй шинэ property нэмэх
+// 2.3 Бүх property-руу хандаж log дээр хэвлэх 
+
+
+// 3. Create an object called 'person' with properties name, age, and address.
+// 3.1 The address property should be an object with properties street, city, and country.
+// 3.2 Access and log the properties of the `address` object. 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
